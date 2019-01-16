a97ab5f43631fa4a97c70e39a077080a6ff76b4a
Uttam Sharma <uttams@bsf.io>
2019-01-16 17:22:59 +0530

    added import export functionality

--- inc/classes/class-astra-import-export-loader.php
+++ inc/classes/class-astra-import-export-loader.php
@@ -52,8 +52,9 @@ if ( ! class_exists( 'Astra_Import_Export_Loader' ) ) {
 		 */
 		public function __construct() {
 
-			add_action( 'customize_register', array( $this, 'customize_register' ) );
-			add_action( 'customize_controls_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+			add_action( 'astra_welcome_page_right_sidebar_content', array( $this, 'astra_import_export_section' ), 50 );
+			add_action( 'admin_init',					array( $this, 'export' ) );
+			add_action( 'admin_init',					array( $this, 'import' ) );
 		}
 
 		/**
@@ -61,350 +62,121 @@ if ( ! class_exists( 'Astra_Import_Export_Loader' ) ) {
 		 *
 		 * @param WP_Customize_Manager $wp_customize Theme Customizer object.
 		 */
-		function customize_register( $wp_customize ) {
+		function astra_import_export_section( ) {
+			?>
+			<div class="postbox" id="astra-ie">
+				<h3 class="hndle"><?php _e( 'Export Settings', 'astra' );?></h3>
+				<div class="inside">
+					<form method="post">
+						<p><input type="hidden" name="astra_ie_action" value="export_settings" /></p>
+						<p style="margin-bottom:0">
+							<?php wp_nonce_field( 'astra_export_nonce', 'astra_export_nonce' ); ?>
+							<?php submit_button( __( 'Export', 'astra' ), 'button', 'submit', false, array( 'id' => '' ) ); ?>
+						</p>
+					</form>
+				</div>
+			</div>
 
-			if ( ! defined( 'ASTRA_THEME_SETTINGS' ) ) {
-				return;
-			}
+			<div class="postbox" id="astra-ie">
+				<h3 class="hndle"><?php _e( 'Import Settings', 'astra' );?></h3>
+				<div class="inside">
+					<form method="post" enctype="multipart/form-data">
+						<p>
+							<input type="file" name="import_file"/>
+						</p>
+						<p style="margin-bottom:0">
+							<input type="hidden" name="astra_ie_action" value="import_settings" />
+							<?php wp_nonce_field( 'astra_import_nonce', 'astra_import_nonce' ); ?>
+							<?php submit_button( __( 'Import', 'astra' ), 'button', 'submit', false, array( 'id' => '' ) ); ?>
+						</p>
+					</form>
 
-			if ( current_user_can( 'edit_theme_options' ) ) {
-
-				if ( isset( $_REQUEST['astra-export'] ) ) {
-					self::_export( $wp_customize );
-				}
-				if ( isset( $_REQUEST['astra-import'] ) && isset( $_FILES['astra-import-file'] ) ) {
-					self::_import( $wp_customize );
-				}
-			}
-
-			/**
-			 * Register Sections & Panels
-			 */
-			require_once ASTRA_IMPORT_EXPORT_DIR . 'inc/classes/customizer-panels-and-sections.php';
-
-			/**
-			 * Sections
-			 */
-			require_once ASTRA_IMPORT_EXPORT_DIR . 'inc/classes/sections/section-banner.php';
+				</div>
+			</div>
+			<?php
 		}
 
 		/**
-		 * Customizer Preview
-		 */
-		function enqueue_scripts() {
-
-			wp_register_style( 'astra-import-export-css', ASTRA_IMPORT_EXPORT_URI . 'inc/assets/css/customizer.css', array(), ASTRA_IMPORT_EXPORT_VER );
-			wp_register_script( 'astra-import-export-js', ASTRA_IMPORT_EXPORT_URI . 'inc/assets/js/customizer.js', array( 'jquery' ), ASTRA_IMPORT_EXPORT_VER, true );
-			// Localize
-			wp_localize_script( 'astra-import-export-js', 'ASTRAl10n', array(
-				'emptyImport'	=> __( 'Please choose a file to import.', 'customizer-export-import' )
-			));
-
-			// Config
-			wp_localize_script( 'astra-import-export-js', 'ASTRAConfig', array(
-				'customizerURL'	  => admin_url( 'customize.php' ),
-				'exportNonce'	  => wp_create_nonce( 'astra-exporting' )
-			));
-
-			// Enqueue
-			wp_enqueue_style( 'astra-import-export-css' );
-			wp_enqueue_script( 'astra-import-export-js' );
-		}
-
-		/**
-		 * Export customizer settings.
+		 * Import our exported file.
 		 *
-		 * @since 0.1
-		 * @since 0.3 Added $wp_customize param and exporting of options.
-		 * @access private
-		 * @param object $wp_customize An instance of WP_Customize_Manager.
-		 * @return void
+		 * @since 1.7
 		 */
-		static private function _export( $wp_customize )
-		{
-			if ( ! wp_verify_nonce( $_REQUEST['astra-export'], 'astra-exporting' ) ) {
+		public static function import() {
+			if ( empty( $_POST['astra_ie_action'] ) || 'import_settings' != $_POST['astra_ie_action'] ) {
 				return;
 			}
 
-			$theme		= get_stylesheet();
-			$template	= get_template();
-			$charset	= get_option( 'blog_charset' );
-			$mods		= get_theme_mods();
-			$data		= array(
-							  'template'  => $template,
-							  'mods'	  => $mods ? $mods : array(),
-							  'options'	  => array()
-						  );
+			if ( ! wp_verify_nonce( $_POST['astra_import_nonce'], 'astra_import_nonce' ) ) {
+				return;
+			}
 
-			require_once ASTRA_IMPORT_EXPORT_DIR . 'inc/classes/array2xml.php';
+			if ( ! current_user_can( 'manage_options' ) ) {
+				return;
+			}
+
+			$filename = $_FILES['import_file']['name'];
+			$extension = end( explode( '.', $_FILES['import_file']['name'] ) );
+
+			if ( $extension != 'json' ) {
+				wp_die( __( 'Please upload a valid .json file', 'gp-premium' ) );
+			}
+
+			$import_file = $_FILES['import_file']['tmp_name'];
+
+			if ( empty( $import_file ) ) {
+				wp_die( __( 'Please upload a file to import', 'gp-premium' ) );
+			}
+
+			// Retrieve the settings from the file and convert the json object to an array.
+			$settings = json_decode( file_get_contents( $import_file ), true );
+
+			$astra_theme_options = get_option( 'astra-settings' );
+			
+			// Delete existing dynamic CSS cache
+			delete_option( 'astra-settings' );
+			
+			update_option( 'astra-settings', $settings );
+
+			wp_safe_redirect( admin_url( 'admin.php?page=astra&status=imported' ) );
+			exit;
+		}
+
+		/**
+		 * Export our chosen options.
+		 *
+		 * @since 1.7
+		 */
+		public static function export( ) {
+
+			if ( empty( $_POST['astra_ie_action'] ) || 'export_settings' != $_POST['astra_ie_action'] ) {
+				return;
+			}
+
+			if ( ! wp_verify_nonce( $_POST['astra_export_nonce'], 'astra_export_nonce' ) ) {
+				return;
+			}
+
+			if ( ! current_user_can( 'manage_options' ) ) {
+				return;
+			}
 
 			// Get options from the Customizer API.
-			$settings = $wp_customize->settings();
 
-			foreach ( $settings as $key => $setting ) {
+			$theme_options = Astra_Theme_Options::get_options();
+			
+			$encode = json_encode( $theme_options );
 
-				if ( 'option' == $setting->type ) {
+			nocache_headers();
+			header( 'Content-Type: application/json; charset=utf-8' );
+			header( 'Content-Disposition: attachment; filename=astra-settings-export-' . date( 'm-d-Y' ) . '.json' );
+			header( "Expires: 0" );
 
-					// Don't save widget data.
-					if ( 'widget_' === substr( strtolower( $key ), 0, 7 ) ) {
-						continue;
-					}
-
-					// Don't save sidebar data.
-					if ( 'sidebars_' === substr( strtolower( $key ), 0, 9 ) ) {
-						continue;
-					}
-
-					// Don't save core options.
-					if ( in_array( $key, self::$core_options ) ) {
-						continue;
-					}
-
-					$data['options'][ $key ] = $setting->value();
-				}
-			}
-
-			// Plugin developers can specify additional option keys to export.
-			$option_keys = apply_filters( 'aie_export_option_keys', array() );
-
-			foreach ( $option_keys as $option_key ) {
-				$data['options'][ $option_key ] = get_option( $option_key );
-			}
-
-			if( function_exists( 'wp_get_custom_css_post' ) ) {
-				$data['wp_css'] = wp_get_custom_css();
-			}
-
-			// Set the download headers.
-			header( 'Content-disposition: attachment; filename=' . $theme . '-export.xml' );
-			header( 'Content-Type: text/xml; charset=' . $charset );
-
-			$xml = Array2XML::createXML('root-element-here', $data);
-  
-  			echo $xml->saveXML();
-
-			// Serialize the export data.
-			// echo serialize( $data );
+			echo $encode;
 
 			// Start the download.
 			die();
 		}
-
-		/**
-		 * Imports uploaded mods and calls WordPress core customize_save actions so
-		 * themes that hook into them can act before mods are saved to the database.
-		 *
-		 * @since 0.1
-		 * @since 0.3 Added $wp_customize param and importing of options.
-		 * @access private
-		 * @param object $wp_customize An instance of WP_Customize_Manager.
-		 * @return void
-		 */
-		static private function _import( $wp_customize )
-		{
-			// Make sure we have a valid nonce.
-			if ( ! wp_verify_nonce( $_REQUEST['astra-import'], 'astra-importing' ) ) {
-				return;
-			}
-
-			// Make sure WordPress upload support is loaded.
-			if ( ! function_exists( 'wp_handle_upload' ) ) {
-				require_once( ABSPATH . 'wp-admin/includes/file.php' );
-			}
-
-			// Load the export/import option class.
-			require_once ASTRA_IMPORT_EXPORT_DIR . 'inc/classes/class-astra-ie-option.php';
-
-			// Setup global vars.
-			global $wp_customize;
-			global $AIE_error;
-
-			// Setup internal vars.
-			$AIE_error	 = false;
-			$template	 = get_template();
-			$overrides   = array( 'test_form' => false, 'test_type' => false, 'mimes' => array('dat' => 'text/plain') );
-			$file        = wp_handle_upload( $_FILES['astra-import-file'], $overrides );
-
-			// Make sure we have an uploaded file.
-			if ( isset( $file['error'] ) ) {
-				$AIE_error = $file['error'];
-				return;
-			}
-			if ( ! file_exists( $file['file'] ) ) {
-				$AIE_error = __( 'Error importing settings! Please try again.', 'customizer-export-import' );
-				return;
-			}
-
-			// Get the upload data.
-			$raw  = file_get_contents( $file['file'] );
-			$data = @unserialize( $raw );
-
-			// Remove the uploaded file.
-			unlink( $file['file'] );
-
-			// Data checks.
-			if ( 'array' != gettype( $data ) ) {
-				$AIE_error = __( 'Error importing settings! Please check that you uploaded a customizer export file.', 'customizer-export-import' );
-				return;
-			}
-			if ( ! isset( $data['template'] ) || ! isset( $data['mods'] ) ) {
-				$AIE_error = __( 'Error importing settings! Please check that you uploaded a customizer export file.', 'customizer-export-import' );
-				return;
-			}
-			if ( $data['template'] != $template ) {
-				$AIE_error = __( 'Error importing settings! The settings you uploaded are not for the current theme.', 'customizer-export-import' );
-				return;
-			}
-
-			// Import images.
-			if ( isset( $_REQUEST['astra-import-images'] ) ) {
-				$data['mods'] = self::_import_images( $data['mods'] );
-			}
-
-			// Import custom options.
-			if ( isset( $data['options'] ) ) {
-
-				foreach ( $data['options'] as $option_key => $option_value ) {
-
-					$option = new Astra_IE_Option( $wp_customize, $option_key, array(
-						'default'		=> '',
-						'type'			=> 'option',
-						'capability'	=> 'edit_theme_options'
-					) );
-
-					$option->import( $option_value );
-				}
-			}
-
-			// If wp_css is set then import it.
-			if( function_exists( 'wp_update_custom_css_post' ) && isset( $data['wp_css'] ) && '' !== $data['wp_css'] ) {
-				wp_update_custom_css_post( $data['wp_css'] );
-			}
-
-			// Call the customize_save action.
-			do_action( 'customize_save', $wp_customize );
-
-			// Loop through the mods.
-			foreach ( $data['mods'] as $key => $val ) {
-
-				// Call the customize_save_ dynamic action.
-				do_action( 'customize_save_' . $key, $wp_customize );
-
-				// Save the mod.
-				set_theme_mod( $key, $val );
-			}
-
-			// Call the customize_save_after action.
-			do_action( 'customize_save_after', $wp_customize );
-		}
-
-		/**
-		 * Imports images for settings saved as mods.
-		 *
-		 * @since 0.1
-		 * @access private
-		 * @param array $mods An array of customizer mods.
-		 * @return array The mods array with any new import data.
-		 */
-		static private function _import_images( $mods )
-		{
-			foreach ( $mods as $key => $val ) {
-
-				if ( self::_is_image_url( $val ) ) {
-
-					$data = self::_sideload_image( $val );
-
-					if ( ! is_wp_error( $data ) ) {
-
-						$mods[ $key ] = $data->url;
-
-						// Handle header image controls.
-						if ( isset( $mods[ $key . '_data' ] ) ) {
-							$mods[ $key . '_data' ] = $data;
-							update_post_meta( $data->attachment_id, '_wp_attachment_is_custom_header', get_stylesheet() );
-						}
-					}
-				}
-			}
-
-			return $mods;
-		}
-
-		/**
-		 * Taken from the core media_sideload_image function and
-		 * modified to return an array of data instead of html.
-		 *
-		 * @since 0.1
-		 * @access private
-		 * @param string $file The image file path.
-		 * @return array An array of image data.
-		 */
-		static private function _sideload_image( $file )
-		{
-			$data = new stdClass();
-
-			if ( ! function_exists( 'media_handle_sideload' ) ) {
-				require_once( ABSPATH . 'wp-admin/includes/media.php' );
-				require_once( ABSPATH . 'wp-admin/includes/file.php' );
-				require_once( ABSPATH . 'wp-admin/includes/image.php' );
-			}
-			if ( ! empty( $file ) ) {
-
-				// Set variables for storage, fix file filename for query strings.
-				preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $file, $matches );
-				$file_array = array();
-				$file_array['name'] = basename( $matches[0] );
-
-				// Download file to temp location.
-				$file_array['tmp_name'] = download_url( $file );
-
-				// If error storing temporarily, return the error.
-				if ( is_wp_error( $file_array['tmp_name'] ) ) {
-					return $file_array['tmp_name'];
-				}
-
-				// Do the validation and storage stuff.
-				$id = media_handle_sideload( $file_array, 0 );
-
-				// If error storing permanently, unlink.
-				if ( is_wp_error( $id ) ) {
-					@unlink( $file_array['tmp_name'] );
-					return $id;
-				}
-
-				// Build the object to return.
-				$meta					= wp_get_attachment_metadata( $id );
-				$data->attachment_id	= $id;
-				$data->url				= wp_get_attachment_url( $id );
-				$data->thumbnail_url	= wp_get_attachment_thumb_url( $id );
-				$data->height			= $meta['height'];
-				$data->width			= $meta['width'];
-			}
-
-			return $data;
-		}
-
-		/**
-		 * Checks to see whether a string is an image url or not.
-		 *
-		 * @since 0.1
-		 * @access private
-		 * @param string $string The string to check.
-		 * @return bool Whether the string is an image url or not.
-		 */
-		static private function _is_image_url( $string = '' )
-		{
-			if ( is_string( $string ) ) {
-
-				if ( preg_match( '/\.(jpg|jpeg|png|gif)/i', $string ) ) {
-					return true;
-				}
-			}
-
-			return false;
-		}
 	}
-
 }
 
 /**
