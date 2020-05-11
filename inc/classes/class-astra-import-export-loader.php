bec2beb097277a09f68d86fa9415e4450e1e5003
imnavanath <navanathb@bsf.io>
2020-05-11 12:05:48 +0530

    Escaping used and resolved phpcs error

--- inc/classes/class-astra-import-export-loader.php
+++ inc/classes/class-astra-import-export-loader.php
@@ -62,7 +62,7 @@ if ( ! class_exists( 'Astra_Import_Export_Loader' ) ) {
 			<div class="postbox" id="astra-ie">
 				<h2 class="hndle ast-normal-cusror"><span class="dashicons dashicons-download"></span><?php esc_html_e( 'Export Settings', 'astra-import-export' ); ?></h2>
 				<div class="inside">
-					<p><?php esc_html_e( 'Export your current ' . $theme_name . ' Customizer settings.', 'astra-import-export' ); ?></p>
+					<p><?php esc_html_e( 'Export your current ' . esc_html( $theme_name ) . ' Customizer settings.', 'astra-import-export' ); // phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralText ?></p>
 
 					<form method="post">
 						<p><input type="hidden" name="astra_ie_action" value="export_settings" /></p>
@@ -77,7 +77,7 @@ if ( ! class_exists( 'Astra_Import_Export_Loader' ) ) {
 			<div class="postbox" id="astra-ie">
 				<h2 class="hndle ast-normal-cusror"><span class="dashicons dashicons-upload"></span><?php esc_html_e( 'Import Settings', 'astra-import-export' ); ?></h2>
 				<div class="inside">
-					<p><?php esc_html_e( 'Import your ' . $theme_name . ' Customizer settings.', 'astra-import-export' ); ?>
+					<p><?php esc_html_e( 'Import your ' . esc_html( $theme_name ) . ' Customizer settings.', 'astra-import-export' ); // phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralText ?>
 
 					<form method="post" enctype="multipart/form-data">
 						<p>
